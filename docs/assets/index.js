eb47b53da0bc076327898cd2552451f2b73e2d31
Psiloclast <me@psiloclast.com>
2021-08-03 07:42:25 -0500

    Display different progress bars for each sub section.

--- docs/assets/index.js
+++ docs/assets/index.js
@@ -1,32 +1,56 @@
-const progressEls = {
-  "project-euler": document.getElementById("project-euler"),
-  "leet-code": document.getElementById("leet-code"),
-  "hacker-rank": document.getElementById("hacker-rank"),
-};
+function getRandomColor() {
+  const hex = "0123456789ABCDEF";
+  let output = "";
+  for (let i = 0; i < 6; ++i) {
+    output += hex.charAt(Math.floor(Math.random() * hex.length));
+  }
+  return "#" + output;
+}
 
-function progress(elem, amount) {
-  var width = 1;
-  var id = setInterval(frame, 10);
+function progress(elem, amount, total) {
+  let width = 0;
+  let id = setInterval(frame, 10);
   function frame() {
     if (width >= amount) {
       clearInterval(id);
     } else {
       width++;
-      elem.style.width = width + "%";
+      elem.style.width = (width / total) * 100 + "%";
     }
   }
 }
 
+function createProgreessBar(amount) {
+  const progressEl = document.createElement("div");
+  progressEl.classList.add("progress");
+  progressEl.style["background-color"] = getRandomColor();
+  // Pick 100 as arbitrary goal for each website
+  progress(progressEl, amount, 100);
+  return progressEl;
+}
+
+function formatHeader(headerStr) {
+  return headerStr
+    .split("-")
+    .map((str) => str[0].toUpperCase() + str.slice(1).toLowerCase())
+    .join(" ");
+}
+
+const barsEl = document.getElementById("bars");
 async function main() {
   const stats = await fetch("stats.json").then((res) => res.json());
-  const progressNames = Object.keys(stats);
-  for (let i = 0; i < progressNames.length; i++) {
-    const name = progressNames[i];
-    const barEl = progressEls[name];
-    const progressEl = barEl.firstElementChild;
-    const amount = stats[name].total;
-    progress(progressEl, amount);
-  }
+  Object.entries(stats).forEach(([websiteName, { sections }]) => {
+    const barHeader = document.createElement("h3");
+    barHeader.innerText = `${formatHeader(websiteName)} xp`;
+    const barEl = document.createElement("div");
+    barEl.classList.add("bar");
+    Object.entries(sections).forEach(([_, amount]) => {
+      const progressEl = createProgreessBar(amount);
+      barEl.appendChild(progressEl);
+    });
+    barsEl.appendChild(barHeader);
+    barsEl.appendChild(barEl);
+  });
 }
 
 main();
