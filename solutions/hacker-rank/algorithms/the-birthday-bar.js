2dc2f8220550a653f5f7aec5ce9876bfc5fa2144
SusieHatter <susiivaldez@gmail.com>
2021-08-05 00:41:35 -0500

    format

--- solutions/hacker-rank/algorithms/the-birthday-bar.js
+++ solutions/hacker-rank/algorithms/the-birthday-bar.js
@@ -1,25 +1,25 @@
-'use strict';
+"use strict";
 
-const fs = require('fs');
+const fs = require("fs");
 
 process.stdin.resume();
-process.stdin.setEncoding('utf-8');
+process.stdin.setEncoding("utf-8");
 
-let inputString = '';
+let inputString = "";
 let currentLine = 0;
 
-process.stdin.on('data', function(inputStdin) {
-    inputString += inputStdin;
+process.stdin.on("data", function (inputStdin) {
+  inputString += inputStdin;
 });
 
-process.stdin.on('end', function() {
-    inputString = inputString.split('\n');
+process.stdin.on("end", function () {
+  inputString = inputString.split("\n");
 
-    main();
+  main();
 });
 
 function readLine() {
-    return inputString[currentLine++];
+  return inputString[currentLine++];
 }
 
 /*
@@ -33,24 +33,26 @@ function readLine() {
  */
 
 function birthday(s, d, m) {
-    let result = 0;
-    for (let i = 0, l = s.length; i < l; i++) {
-        if (s.slice(i, i + m).reduce((x, y) => x + y) === d) {
-            result++;
-        }
+  let result = 0;
+  for (let i = 0, l = s.length; i < l; i++) {
+    if (s.slice(i, i + m).reduce((x, y) => x + y) === d) {
+      result++;
     }
-    return result;
+  }
+  return result;
 }
 
-
 function main() {
-    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
-    const n = parseInt(readLine().trim(), 10);
-    const s = readLine().replace(/\s+$/g, '').split(' ').map(sTemp => parseInt(sTemp, 10));
-    const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
-    const d = parseInt(firstMultipleInput[0], 10);
-    const m = parseInt(firstMultipleInput[1], 10);
-    const result = birthday(s, d, m);
-    ws.write(result + '\n');
-    ws.end();
+  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+  const n = parseInt(readLine().trim(), 10);
+  const s = readLine()
+    .replace(/\s+$/g, "")
+    .split(" ")
+    .map((sTemp) => parseInt(sTemp, 10));
+  const firstMultipleInput = readLine().replace(/\s+$/g, "").split(" ");
+  const d = parseInt(firstMultipleInput[0], 10);
+  const m = parseInt(firstMultipleInput[1], 10);
+  const result = birthday(s, d, m);
+  ws.write(result + "\n");
+  ws.end();
 }
