93ca34681a129d9fa2a3fabbdf3467f71894fec6
SusieHatter <susiivaldez@gmail.com>
2021-08-24 20:26:46 -0500

    Solve 2 problems

---
+++ solutions/hacker-rank/10-days-of-javascript/day-1-let-and-const.js
@@ -0,0 +1,18 @@
+function main() {
+    // Write your code here. Read input using 'readLine()' and print output using 'console.log()'.
+    let r = readLine();
+    const PI =Math.PI;
+    let area = PI * Math.pow(r, 2);
+    let perimeter = 2 * PI * r;
+    console.log(area);
+    console.log(perimeter);
+        
+    try {    
+        // Attempt to redefine the value of constant variable PI
+        PI = 0;
+        // Attempt to print the value of PI
+        console.log(PI);
+    } catch(error) {
+        console.error("You correctly declared 'PI' as a constant.");
+    }
+}
