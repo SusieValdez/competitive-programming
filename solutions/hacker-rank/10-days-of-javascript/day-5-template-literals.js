4c497a427f3b9d8a2044d503558282500e7d985b
SusieHatter <susiivaldez@gmail.com>
2021-08-27 14:19:11 -0500

    Finish day 5 of js

---
+++ solutions/hacker-rank/10-days-of-javascript/day-5-template-literals.js
@@ -0,0 +1,46 @@
+"use strict";
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", (inputStdin) => {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", (_) => {
+  inputString = inputString
+    .trim()
+    .split("\n")
+    .map((string) => {
+      return string.trim();
+    });
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+/*
+ * Determine the original side lengths and return an array:
+ * - The first element is the length of the shorter side
+ * - The second element is the length of the longer side
+ *
+ * Parameter(s):
+ * literals: The tagged template literal's array of strings.
+ * expressions: The tagged template literal's array of expression values (i.e., [area, perimeter]).
+ */
+function sides(literals, ...expressions) {
+  let values = [];
+  const [a, p] = expressions;
+  let operation = Math.sqrt(Math.pow(p, 2) - 16 * a);
+  let s1 = (p + operation) / 4;
+  let s2 = (p - operation) / 4;
+  values.push(s1);
+  values.push(s2);
+  return values.sort();
+}
